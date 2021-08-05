cad15bcf2fe3c63b77ef942e9a93f509ead16337
colin5530 <colinmcdermott90@gmail.com>
2021-08-05 18:12:10 +0100

    Mobile & Cleanup

--- src/pages/ProfilePage/ProfilePage.js
+++ src/pages/ProfilePage/ProfilePage.js
@@ -14,35 +14,35 @@ const ProfilePage = () => {
 
   return (
     <div>
+      <div className='profile-page'>
+        <img src={vehicle.pictureURL} alt='car' className='profile-image' />
+        <div className='profile-name'>{vehicle.name}</div>
+        <div className='profile-details'>
+          <div className='profile-attribute'><span className='profile-attribute-title'>Fuel Type:</span><span>{vehicle.fuelType}</span></div>
+          <div className='profile-attribute'><span className='profile-attribute-title'>Transmission:</span><span>{vehicle.transmission}</span></div>
+          <div className='profile-attribute'><span className='profile-attribute-title'>Doors:</span><span>{vehicle.doorCount}</span></div>
+          <div className='profile-attribute'><span className='profile-attribute-title'>Passengers:</span><span>{vehicle.passengerQuantity}</span></div>
+          <div className='profile-attribute'><span className='profile-attribute-title'>Baggage QTY:</span><span>{vehicle.baggageQuantity}</span></div>
+          <div className='profile-attribute'><span className='profile-attribute-title'>Air Conditioning:</span><span>{vehicle.airConditioning}</span></div>
+          <div className='profile-attribute'><span className='profile-attribute-title'>Drive Type:</span><span>{vehicle.driveType}</span></div>
+          <div className='profile-attribute'><span className='profile-attribute-title'>Vendor:</span><span>{vehicle.vendor.name}</span></div>
+          <div className='profile-attribute'><span className='profile-attribute-title'>Code:</span><span>{vehicle.vendor.code}</span></div>
+          <div className='profile-attribute'><span className='profile-attribute-title'>Code:</span><span>{vehicle.code}</span></div>
+          <div className='profile-attribute'><span className='profile-attribute-title'>Code Context:</span><span>{vehicle.codeContext}</span></div>
+        </div>
+        <div className='profile-footer'>
+          <div className={`profile-availability ${vehicle.status === 'Available' ? 'available' : 'unavailable'}`}>
+            {vehicle.status}
+          </div>
+          <div className='profile-pricing'>
+            <div className='profile-rate-price'><span className='profile-rate-price-title'>Rate:</span> {currencyCodeSymbols[vehicle.currencyCode]}{vehicle.rate}</div>
+            <div className='profile-total-price'><span className='profile-total-price-title'>Estimated Total:</span> {currencyCodeSymbols[vehicle.currencyCode]}{vehicle.estimatedTotal}</div>
+          </div>
+        </div>
+      </div>
       <Link to='/'>
         <button className='back-button'>Back</button>
       </Link>
-    <div className='profile-page'>
-      <img src={vehicle.pictureURL} alt='car' className='profile-image' />
-      <div className='profile-name'>{vehicle.name}</div>
-      <div className='profile-details'>
-        <div className='profile-attribute'><span className='profile-attribute-title'>Fuel Type:</span><span>{vehicle.fuelType}</span></div>
-        <div className='profile-attribute'><span className='profile-attribute-title'>Transmission:</span><span>{vehicle.transmission}</span></div>
-        <div className='profile-attribute'><span className='profile-attribute-title'>Doors:</span><span>{vehicle.doorCount}</span></div>
-        <div className='profile-attribute'><span className='profile-attribute-title'>Passengers:</span><span>{vehicle.passengerQuantity}</span></div>
-        <div className='profile-attribute'><span className='profile-attribute-title'>Baggage QTY:</span><span>{vehicle.baggageQuantity}</span></div>
-        <div className='profile-attribute'><span className='profile-attribute-title'>Air Conditioning:</span><span>{vehicle.airConditioning}</span></div>
-        <div className='profile-attribute'><span className='profile-attribute-title'>Drive Type:</span><span>{vehicle.driveType}</span></div>
-        <div className='profile-attribute'><span className='profile-attribute-title'>Vendor:</span><span>{vehicle.vendor.name}</span></div>
-        <div className='profile-attribute'><span className='profile-attribute-title'>Code:</span><span>{vehicle.vendor.code}</span></div>
-        <div className='profile-attribute'><span className='profile-attribute-title'>Code:</span><span>{vehicle.code}</span></div>
-        <div className='profile-attribute'><span className='profile-attribute-title'>Code Context:</span><span>{vehicle.codeContext}</span></div>
-      </div>
-      <div className='profile-footer'>
-        <div className={`profile-availability ${vehicle.status === 'Available' ? 'available' : 'unavailable'}`}>
-          {vehicle.status}
-        </div>
-        <div className='profile-pricing'>
-          <div className='profile-rate-price'><span className='profile-rate-price-title'>Rate:</span> {currencyCodeSymbols[vehicle.currencyCode]}{vehicle.rate}</div>
-          <div className='profile-total-price'><span className='profile-total-price-title'>Estimated Total:</span> {currencyCodeSymbols[vehicle.currencyCode]}{vehicle.estimatedTotal}</div>
-        </div>
-      </div>
-    </div>
     </div>
   )
 };
